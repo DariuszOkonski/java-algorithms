c65c18d5965d6c5cce9ff8a1429fb6e57e03df0c
Dariusz Okoński <darek200180@gmail.com>
2022-11-18 16:12:23 +0100

    add palindrome problem solution

--- algorithms/src/pac_01_arrays/PalindromeProblem/App.java
+++ algorithms/src/pac_01_arrays/PalindromeProblem/App.java
@@ -4,5 +4,13 @@ public class App {
     public static void main(String[] args) {
         String word1 = "radar";
         String word2 = "madam";
+        String word3 = "hello";
+        String word4 = "raar";
+
+        PalindromeProblem problem = new PalindromeProblem();
+        System.out.println(problem.solve(word1));
+        System.out.println(problem.solve(word2));
+        System.out.println(problem.solve(word3));
+        System.out.println(problem.solve(word4));
     }
 }
--- algorithms/src/pac_01_arrays/PalindromeProblem/PalindromeProblem.java
+++ algorithms/src/pac_01_arrays/PalindromeProblem/PalindromeProblem.java
@@ -1,5 +1,25 @@
 package pac_01_arrays.PalindromeProblem;
 
 public class PalindromeProblem {
+    // it has 0(N) linear running time complexity
+    public boolean solve(String str) {
+        int forward = 0;
+        int backward = str.length() - 1;
 
+        while (forward < backward) {
+            if(str.charAt(forward) != str.charAt(backward))
+                return false;
+            forward++;
+            backward--;
+        }
+        return true;
+    }
+
+//    public boolean solve(String str) {
+//        for (int i = 0; i < str.length() / 2; i++) {
+//            if(str.charAt(i) != str.charAt((str.length() - 1) - i))
+//                return false;
+//        }
+//        return true;
+//    }
 }
